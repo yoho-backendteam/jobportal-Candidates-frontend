19dfde7e0b3bcb83d753606e95e4cd673269f597
ARAVINTH-12345 <aravinth.yoho@gmail.com>
2025-11-24 16:18:34 +0530

    completed

--- src/Constants/uiConstants.ts
+++ src/Constants/uiConstants.ts
@@ -1,31 +1,31 @@
 export const COLORS = {
-    primary_color: '#FC8019',
-    primary_black:"#0A0A0A" ,
-    SignUp_forget:"#717182",
-    Signup_bottom:"#45556C"
+    // primary_color: '#FC8019',
+    // primary_black:"#0A0A0A" ,
+    // SignUp_forget:"#717182",
+    // Signup_bottom:"#45556C"
     
 };
 
 export const FONTS = {
-    SignUPTitle: {
-        fontFamily: 'Arimo', 
-        fontWeight: 400,
-        fontSize: '30px',
-    },
-      SignUPSubTitle: {
-        fontFamily: 'Arimo', 
-        fontWeight: 400,
-        fontSize: '16px',
-    },
-      SignUPLoginTitle: {
-        fontFamily: 'Arimo', 
-        fontWeight: 400,
-        fontSize: '14px',
-    },
-      SignUPForget: {
-        fontFamily: 'Arimo', 
-        fontWeight: 400,
-        fontSize: '12px',
-    },
+    // SignUPTitle: {
+    //     fontFamily: 'Arimo', 
+    //     fontWeight: 400,
+    //     fontSize: '30px',
+    // },
+    //   SignUPSubTitle: {
+    //     fontFamily: 'Arimo', 
+    //     fontWeight: 400,
+    //     fontSize: '16px',
+    // },
+    //   SignUPLoginTitle: {
+    //     fontFamily: 'Arimo', 
+    //     fontWeight: 400,
+    //     fontSize: '14px',
+    // },
+    //   SignUPForget: {
+    //     fontFamily: 'Arimo', 
+    //     fontWeight: 400,
+    //     fontSize: '12px',
+    // },
    
 }
--- src/Pages/Login/SignIn.tsx
+++ src/Pages/Login/SignIn.tsx
@@ -5,18 +5,18 @@ import { COLORS, FONTS } from "../../Constants/uiConstants"
 const SignIn = () => {
   return (
     <div className="">
-      <div style={{ backgroundColor: COLORS.primary_color }} className=" h-screen m-3 rounded-lg">
+      <div className=" h-screen m-3 rounded-lg">
         <div className="min-h-screen flex items-center justify-center ">
           <div className="w-full max-w-sm bg-white p-8 rounded-2xl shadow-lg">
             <div className="flex flex-row  justify-center ">
               <img src={Logo_for_Login} className="w-15 h-15 " alt="" />
             </div>
-            <h2 style={{ ...FONTS.SignUPTitle, color: COLORS.primary_black }} className="text-2xl font-semibold text-center">Sign In</h2>
-            <p style={{ ...FONTS.SignUPSubTitle, color: COLORS.primary_black }} className="flex items-center justify-center ">Join TalentHub  and start your carrer</p>
+            <h2 className="text-2xl font-semibold text-center">Sign In</h2>
+            <p className="flex items-center justify-center ">Join TalentHub  and start your carrer</p>
 
             <form className="space-y-4 mt-5">
               <div>
-                <label style={{ ...FONTS.SignUPLoginTitle, color: COLORS.primary_black }}  className="block mb-1 font-medium">Email?Phone *</label>
+                <label className="block mb-1 font-medium">Email?Phone *</label>
                 <input
                   type="email"
                   placeholder="Enter Your email or phone number"
@@ -26,7 +26,7 @@ const SignIn = () => {
               </div>
 
               <div>
-                <label style={{ ...FONTS.SignUPLoginTitle, color: COLORS.primary_black }} className="block mb-1 font-medium">Password *</label>
+                <label className="block mb-1 font-medium">Password *</label>
                 <input
                   type="password"
                   placeholder="**********"
@@ -36,21 +36,21 @@ const SignIn = () => {
               </div>
 
 
-              <div style={{ ...FONTS.SignUPForget, color: COLORS.SignUp_forget }}  className="flex justify-end">
+              <div className="flex justify-end">
                 <label className="block mb-1 font-medium">Forget Password?</label>
               </div>
 
 
               <button
-              style={{ ...FONTS.SignUPLoginTitle, backgroundColor:COLORS.primary_color }} 
+
                 type="submit"
                 className="w-full py-2 mt-3  text-white rounded-lg transition"
               >
                 Sign In
               </button>
             </form>
-            <div  className="mt-5">
-              <p className="flex items-center justify-center" style={{ ...FONTS.SignUPLoginTitle, color: COLORS.Signup_bottom}}>Don"t have an account? <span style={{ ...FONTS.SignUPSubTitle}} className="text-[#0052CC]">Sign Up</span></p>
+            <div className="mt-5">
+              <p className="flex items-center justify-center" >Don"t have an account? <span className="text-[#0052CC]">Sign Up</span></p>
             </div>
 
           </div>

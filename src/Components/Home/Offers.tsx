dc7348c4c89fc88db413bc31371ac9a222629c59
Prakashraj2404 <prakashmadhaiyan02@gmail.com>
2025-11-25 13:02:30 +0530

    corrections

--- src/Components/Home/Offers.tsx
+++ src/Components/Home/Offers.tsx
@@ -1,73 +1,78 @@
-import dollar from "../../assets/Home/Dollar_Offer.png"
-import location from "../../assets/Home/Location.png"
-import department from "../../assets/Home/Department.png"
-import date from "../../assets/Home/date.png"
-import employeement from "../../assets/Home/Employeetype.png"
-import user from "../../assets/Home/Offer_User.png"
-import offer from "../../assets/Home/Offer_Letter_icon.png"
+import dollar from "../../assets/Home/Dollar_Offer.png";
+import location from "../../assets/Home/Location.png";
+import department from "../../assets/Home/Department.png";
+import date from "../../assets/Home/date.png";
+import employeement from "../../assets/Home/Employeetype.png";
+import user from "../../assets/Home/Offer_User.png";
+import offer from "../../assets/Home/Offer_Letter_icon.png";
 import { LuDownload } from "react-icons/lu";
-import Accept from "../../assets/Home/Home_Offer_accept.png"
-import Decline from "../../assets/Home/Home_Offer_decline.png"
-import { useState } from "react"
-import Success from "../../assets/Home/Confirmation_icon.png"
-import decline from "../../assets/Home/Confirmation_decline_icon.png"
-import doubletick from "../../assets/Home/Double_tick.png"
+import Accept from "../../assets/Home/Home_Offer_accept.png";
+import Decline from "../../assets/Home/Home_Offer_decline.png";
+import { useState } from "react";
+import Success from "../../assets/Home/Confirmation_icon.png";
+import decline from "../../assets/Home/Confirmation_decline_icon.png";
+import doubletick from "../../assets/Home/Double_tick.png";
 
 const Offers = () => {
-  const [page, setpage] = useState(false)
-  const [declinepage, setdeclinepage] = useState(false)
-  const [success, setsuccess] = useState(false)
+  const [page, setpage] = useState(false);
+  const [declinepage, setdeclinepage] = useState(false);
+  const [success, setsuccess] = useState(false);
   return (
     <div>
-      <div className='flex-flex-col gap-4 bg-[#FF5200] text-white mt-10 rounded-t-2xl p-5'>
-        <p className='flex justify-between'>Senior Frontend Engineer<span className='bg-[#FC8019] text-white px-2 py-1 rounded-full'>Pending Response</span></p>
+      <div className="flex-flex-col gap-4 bg-[#FF5200] text-white mt-10 rounded-t-2xl p-5">
+        <p className="flex justify-between">
+          Senior Frontend Engineer
+          <span className="bg-[#FC8019] text-white px-2 py-1 rounded-full">
+            Pending Response
+          </span>
+        </p>
         <p>TechCorp India </p>
       </div>
 
-      <div className='md:flex md:flex-row md:justify-between   p-5 mt-5'>
-        <div className='w-full flex flex-col gap-4'>
+      <div className="md:flex md:flex-row md:justify-between   p-5 mt-5">
+        <div className="w-full flex flex-col gap-4">
           <div className="flex flex-row items-center gap-2">
             <img src={dollar} className="w-15" alt="" />
             <div>
-              <p>Compensation</p>
+              <p className="text-[#686B78]">Compensation</p>
               <p>22LPA</p>
             </div>
           </div>
           <div className="flex flex-row items-center gap-2">
             <img src={location} className="w-15" alt="" />
             <div>
-              <p>Location</p>
+              <p className="text-[#686B78]">Location</p>
               <p>Bangalore,Karnataka</p>
             </div>
           </div>
           <div className="flex flex-row items-center gap-2">
             <img src={department} className="w-15" alt="" />
             <div>
-              <p>Department</p>
+              <p className="text-[#686B78]">Department</p>
               <p>Engineering</p>
             </div>
           </div>
         </div>
-        <div className='w-full'>
-          <div className='w-full flex flex-col gap-4'>
+        <div className="w-full">
+          <div className="w-full flex flex-col gap-4">
             <div className="flex flex-row items-center gap-2">
               <img src={date} className="w-15" alt="" />
               <div>
-                <p>Joining Date</p>
+                <p className="text-[#686B78]">Joining Date</p>
                 <p>15 February 2025</p>
               </div>
             </div>
             <div className="flex flex-row items-center gap-2">
               <img src={employeement} className="w-15" alt="" />
               <div>
-                <p>Employement Type</p>
+                <p className="text-[#686B78]">Employement Type</p>
                 <p>Full-Type</p>
               </div>
             </div>
             <div className="flex flex-row items-center gap-2">
               <img src={user} className="w-15" alt="" />
               <div>
-                <p>Reporting To</p>
+                <p className="text-[#686B78]">Reporting To</p>
                 <p>Prakash (VP Engineering)</p>
               </div>
             </div>
@@ -75,35 +80,58 @@ const Offers = () => {
         </div>
       </div>
 
-      {!success && <div className="flex flex-row p-2 border-[#E9E9EB] bg-[#F9F9F9] gap-5 rounded-xl">
-        <img src={offer} className=" w-12 h-12" alt="" />
-        <div className="flex flex-row justify-between items-center   w-full">
-          <div className=" ">
-            <p>Offer Letter</p>
-            <p className="text-gray-400">Official offer Documet</p>
-          </div>
-          <div className="flex flex-row items-center gap-5 border border-[#E9E9EB] rounded-lg p-1.5">
-            <LuDownload />
-            <button>
-              Download
-            </button>
+      {!success && (
+        <div className="flex flex-row p-2 border-[#E9E9EB] bg-[#F9F9F9] gap-5 rounded-xl">
+          <img src={offer} className=" w-12 h-12" alt="" />
+          <div className="flex flex-row justify-between items-center   w-full">
+            <div className=" ">
+              <p>Offer Letter</p>
+              <p className="text-gray-400">Official offer Documet</p>
+            </div>
+            <div className="flex flex-row items-center gap-5 border border-[#E9E9EB] rounded-lg p-1.5">
+              <LuDownload />
+              <button>Download</button>
+            </div>
           </div>
         </div>
-      </div>}
+      )}
 
-      {success && <div className="border border-[#60B24633] bg-[#60B2461A] flex  gap-5 rounded-xl px-5 py-2">
-       <img src={doubletick} className="w-10" alt="" />
-        <div>
-          <p className="text-2xl text-gray-800 font-bold">Offer Accept Successfully!</p>
-          <p className="text-md text-gray-400">Plesae upload required document to complete your onboarding</p>
+      {success && (
+        <div className="border border-[#60B24633] bg-[#60B2461A] flex  gap-5 rounded-xl px-5 py-2">
+          <img src={doubletick} className="w-10" alt="" />
+          <div>
+            <p className="text-2xl text-gray-800 font-bold">
+              Offer Accept Successfully!
+            </p>
+            <p className="text-md text-gray-400">
+              Plesae upload required document to complete your onboarding
+            </p>
+          </div>
         </div>
-      </div>}
+      )}
 
-      <div className="flex gap-5 mt-10">
-        <button onClick={() => setpage(true)} className="w-full rounded-lg bg-[#60B246] flex items-center justify-center text-white gap-2"><span><img src={Accept} alt="" className="w-10 p-2" /></span> Accept Offer</button>
-        <button onClick={() => setdeclinepage(true)} className="w-full rounded-lg text-[#E7000B] flex items-center justify-center border border-[#E9E9EB] "><span><img src={Decline} alt="" className="w-10 p-2" /></span>Decline Offer</button>
-      </div>
-
+      {success == false && (
+        <div className="flex gap-5 mt-10">
+          <button
+            onClick={() => setpage(true)}
+            className="w-full rounded-lg bg-[#60B246] flex items-center justify-center text-white gap-2"
+          >
+            <span>
+              <img src={Accept} alt="" className="w-10 p-2" />
+            </span>{" "}
+            Accept Offer
+          </button>
+          <button
+            onClick={() => setdeclinepage(true)}
+            className="w-full rounded-lg text-[#E7000B] flex items-center justify-center border border-[#E9E9EB] "
+          >
+            <span>
+              <img src={Decline} alt="" className="w-10 p-2" />
+            </span>
+            Decline Offer
+          </button>
+        </div>
+      )}
 
       {page && (
         <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-4">
@@ -113,7 +141,8 @@ const Offers = () => {
             </div>
             <p className="text-2xl font-bold text-center">Accept Offer</p>
             <p className="text-md text-gray-500 text-center">
-              Are you sure want to accept this offer? This action will notify the HR team
+              Are you sure want to accept this offer? This action will notify
+              the HR team
             </p>
 
             <div className="flex gap-5 mt-5">
@@ -123,14 +152,16 @@ const Offers = () => {
               >
                 Cancel
               </button>
-              <button onClick={() => (setsuccess(true), setpage(false))} className="w-full rounded-lg bg-[#60B246] text-white p-2">
+              <button
+                onClick={() => (setsuccess(true), setpage(false))}
+                className="w-full rounded-lg bg-[#60B246] text-white p-2"
+              >
                 Yes, Accept
               </button>
             </div>
           </div>
         </div>
       )}
-
 
       {declinepage && (
         <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-4">
@@ -140,7 +171,8 @@ const Offers = () => {
             </div>
             <p className="text-2xl font-bold text-center">Decline Offer</p>
             <p className="text-md text-gray-500 text-center">
-              Are you sure want to decline this offer? This action will notify the HR team
+              Are you sure want to decline this offer? This action will notify
+              the HR team
             </p>
 
             <div className="flex gap-5 mt-5">
@@ -157,12 +189,8 @@ const Offers = () => {
           </div>
         </div>
       )}
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Offers
+export default Offers;

8258cca68c91b16fb610653f48e78c900436487e
Prakashraj2404 <prakashmadhaiyan02@gmail.com>
2025-11-27 18:12:30 +0530

    interview integrations

--- src/Components/Home/AllApplication.tsx
+++ src/Components/Home/AllApplication.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 import ApplicationIcon from "../../assets/Home/ApplicationIcon.png";
 import { FaArrowRight } from "react-icons/fa";
 import { HiLightningBolt } from "react-icons/hi";

bee819ad71285fa76eba0f8af0e6eead63daec18
Prakashraj2404 <prakashmadhaiyan02@gmail.com>
2025-11-27 14:40:29 +0530

    corrections

--- src/features/applications/reducers/selector.tsx
+++
@@ -1,6 +0,0 @@
-import type { RootState } from '../../../store/store';
-
-export const selectAllapplication = (state: RootState) =>
-	state.applicationReducer.applications;
-export const selectedapplication = (state: RootState) =>
-	state.applicationReducer.selectedapplication;
